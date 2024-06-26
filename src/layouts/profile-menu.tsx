024b71031598f7acfbd0dd52f6669dc60dee4d5f
GeorgeMightyfin <gmunganga@mightyfinance.co.zm>
2024-06-26 19:30:17 +0200

    fixes

--- src/layouts/profile-menu.tsx
+++ src/layouts/profile-menu.tsx
@@ -84,38 +84,38 @@ const menuItems = [
 function DropdownMenu() {
   return (
     <div className="w-64 text-left rtl:text-right">
-      <div className="flex items-center border-b border-gray-300 px-6 pb-5 pt-6">
-        <Avatar
-          src="https://isomorphic-furyroad.s3.amazonaws.com/public/avatars-blur/avatar-11.webp"
-          name="Albert Flores"
-        />
-        <div className="ms-3">
-          <Title as="h6" className="font-semibold">
-            Albert Flores
-          </Title>
-          <Text className="text-gray-600">flores@doe.io</Text>
-        </div>
-      </div>
-      <div className="grid px-3.5 py-3.5 font-medium text-gray-700">
-        {menuItems.map((item) => (
-          <Link
-            key={item.name}
-            href={item.href}
-            className="group my-0.5 flex items-center rounded-md px-2.5 py-2 hover:bg-gray-100 focus:outline-none hover:dark:bg-gray-50/50"
-          >
-            {item.name}
-          </Link>
-        ))}
-      </div>
-      <div className="border-t border-gray-300 px-6 pb-6 pt-5">
-        <Button
-          className="h-auto w-full justify-start p-0 font-medium text-gray-700 outline-none focus-within:text-gray-600 hover:text-gray-900 focus-visible:ring-0"
-          variant="text"
-          onClick={() => signOut()}
-        >
-          Sign Out
-        </Button>
+    <div className="flex items-center border-b border-gray-300 px-6 pb-5 pt-6">
+      <Avatar
+        src="https://isomorphic-furyroad.s3.amazonaws.com/public/avatars-blur/avatar-11.webp"
+        name="George Munganga"
+      />
+      <div className="ms-3">
+        <Title as="h6" className="font-semibold">
+          George Munganga
+        </Title>
+        <Text className="text-gray-600">georgemunganga@gmail.com</Text>
       </div>
     </div>
+    <div className="grid px-3.5 py-3.5 font-medium text-gray-700">
+      {menuItems.map((item) => (
+        <Link
+          key={item.name}
+          href={item.href}
+          className="group my-0.5 flex items-center rounded-md px-2.5 py-2 hover:bg-gray-100 focus:outline-none hover:dark:bg-gray-50/50"
+        >
+          {item.name}
+        </Link>
+      ))}
+    </div>
+    <div className="border-t border-gray-300 px-6 pb-6 pt-5">
+      <Button
+        className="h-auto w-full justify-start p-0 font-medium text-gray-700 outline-none focus-within:text-gray-600 hover:text-gray-900 focus-visible:ring-0"
+        variant="text"
+        onClick={() => signOut()}
+      >
+        Sign Out
+      </Button>
+    </div>
+  </div>
   );
 }
