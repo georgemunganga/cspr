b3cdccf98537e37525877e60676e433797e38083
GeorgeMightyfin <gmunganga@mightyfinance.co.zm>
2024-06-26 19:33:31 +0200

    fixes

--- src/app/shared/file/dashboard/index.tsx
+++ src/app/shared/file/dashboard/index.tsx
@@ -71,31 +71,29 @@ const FileDashboard: React.FC<DashboardProps> = ({ data, errors }) => {
   // Render the dashboard if data is available
   return (
     <div className="container">
-      <FileStats className="mb-5 2xl:mb-8" />
-      <div className="mb-6 grid grid-cols-1 gap-6 4xl:grid-cols-12 2xl:mb-8 2xl:gap-8">
-        <StorageReport className="container 4xl:col-span-8 96.937rem:col-span-9" />
-      
-      </div>
-      <div className="grid grid-cols-1 gap-6 container lg:grid-cols-12 2xl:gap-8">
-        <div className="col-span-full flex flex-col gap-6 5xl:col-span-12 2xl:gap-8 3xl:col-span-12">
-          <Provinces data={fileData} />
-        </div>
-      </div>
-      <div className="grid grid-cols-1 gap-6 container lg:grid-cols-12 2xl:gap-8">
-        <div className="col-span-full flex flex-col gap-6 5xl:col-span-8 2xl:gap-8 3xl:col-span-9">
-          <Provinces data={JSON.stringify(fileData)} />
-          <QuickAccess />
-          <RecentFiles />
-          <ActivityReport />
-          <FileListTable />
-        </div>
-        <div className="col-span-full flex flex-col gap-6 5xl:col-span-4 2xl:gap-8 3xl:col-span-3">
-          <RecentActivities />
-          <Members />
-          <UpgradeStorage />
-        </div>
+    <FileStats className="mb-5 2xl:mb-8" />
+    <div className="mb-6 grid grid-cols-1 gap-6 4xl:grid-cols-12 2xl:mb-8 2xl:gap-8">
+      <StorageReport className="container 4xl:col-span-8 96.937rem:col-span-9" />
+    
+    </div>
+    <div className="grid grid-cols-1 gap-6 container lg:grid-cols-12 2xl:gap-8">
+      <div className="col-span-full flex flex-col gap-6 5xl:col-span-12 2xl:gap-8 3xl:col-span-12">
       </div>
     </div>
+    <div className="grid grid-cols-1 gap-6 container lg:grid-cols-12 2xl:gap-8">
+      <div className="col-span-full flex flex-col gap-6 5xl:col-span-8 2xl:gap-8 3xl:col-span-9">
+        <Provinces data={JSON.stringify(fileData)} />
+        <RecentFiles />
+        <ActivityReport />
+        <FileListTable />
+      </div>
+      <div className="col-span-full flex flex-col gap-6 5xl:col-span-4 2xl:gap-8 3xl:col-span-3">
+        <RecentActivities />
+        <Members />
+        <UpgradeStorage />
+      </div>
+    </div>
+  </div>
   );
 };
 
