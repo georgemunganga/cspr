0668cb93e5ffc0eed29e0aec91bab11820dd2bfd
GeorgeMightyfin <gmunganga@mightyfinance.co.zm>
2024-06-26 19:11:07 +0200

    fixes

--- src/components/settings/settings-drawer.tsx
+++ src/components/settings/settings-drawer.tsx
@@ -14,28 +14,13 @@ export default function SettingsDrawer() {
       <SimpleBar className="h-[calc(100%-138px)]">
         <div className="px-5 py-6">
           <ThemeSwitcher />
-          <AppDirection />
-          <LayoutSwitcher />
+        
+          
           <ColorOptions />
         </div>
       </SimpleBar>
 
-      <SettingsFooterButton />
+      
     </>
-  );
-}
-
-function SettingsFooterButton() {
-  return (
-    <a
-      href="https://themeforest.net/item/isomorphic-react-redux-admin-dashboard/20262330?ref=redqteam"
-      target="_blank"
-      className="grid grid-cols-1 border-t border-muted px-6 pt-4"
-    >
-      <Button size="lg" as="span" className={'text-base font-semibold'}>
-        <EnvatoIcon className="me-2 h-5 w-5" />
-        <span className="">Purchase for $24</span>
-      </Button>
-    </a>
   );
 }
